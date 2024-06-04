48ffc8930944e6c295b8cfe745db78fb3c691f94
Ravier Estevam <166070960+raviersilva16@users.noreply.github.com>
2024-06-04 20:57:36 -0300

    tarefa concluida

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2){
+    if(player1 === player2){
+        return "Empate!";
+    }else if((player1 === "Pedra" && player2 === "Tesoura") ||
+        (player1 === "Tesoura" && player2 === "Papel") ||
+        (player1 === "Papel" && player2 === "Pedra")){
+        return "Jogador 1 venceu!";
+
+    }else(player2 === "Pedra" && player1 === "Tesoura") ||
+    (player2 === "Tesoura" && player1 === "Papel") ||
+    (player2 === "Papel" && player1 === "Pedra")
+    return "Jogador 2 venceu!";
+    }
+
